6a165c2b590a108f8d7272ff10d4ae678dc678ae
kabi02 <108052970+kabi02@users.noreply.github.com>
2025-05-04 20:25:01 +0800

    updated dotfiles

--- ohmyposh/Microsoft.PowerShell_profile.ps1
+++ ohmyposh/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,17 @@
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/dracula.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/emodipt-extend.omp.json' | Invoke-Expression
+
+# Yazi YAZI_FILE_ONE directory
+
+$Env:YAZI_FILE_ONE = 'C:\Program Files\Git\usr\bin\file.exe'
+
+# Yazi PowerShell function
+
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    }
+    Remove-Item -Path $tmp
+}
