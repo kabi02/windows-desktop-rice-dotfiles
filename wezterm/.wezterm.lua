aa0255f1e6ae9662c855d50b013e2e1e700c18b5
kabi02 <108052970+kabi02@users.noreply.github.com>
2025-10-15 17:31:42 +0800

    updated yasb configs

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,7 +6,21 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = 'gruvbox_material_dark_medium'
+config.color_schemes = {
+    ["gruvbox_material_dark_medium"] = {
+        foreground = "#D4BE98",
+        background = "#282828",
+        cursor_bg = "#D4BE98",
+        cursor_border = "#D4BE98",
+        cursor_fg = "#282828",
+        selection_bg = "#D4BE98",
+        selection_fg = "#45403d",
+        
+        ansi = { "#282828", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
+        brights = { "#eddeb5", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
+    },
+}
 config.default_cursor_style = 'SteadyUnderline'
 config.underline_thickness = "3px"
 config.default_prog = { 'pwsh.exe' }
